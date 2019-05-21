686c6a01182363a211ffe13ce0ef1c91f8a7cdab
Aaron Hatch <aaron@Aarons-MacBook-Pro.local>
2019-05-20 20:22:00 -0700

    created a callback function

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const friend = "0 > - <";
 const friendList = [];
 
 let mySearch = () => {
-    alert('Searching')
+    alert('Searching');
+    createTextFriend();
+}
+
+let createTextFriend = () => {
     let createNewFriend = document.createElement('p');
     document.body.appendChild(createNewFriend);
     createNewFriend.innerText = friend;
-
-
 }
