b76f5c61ae4366e133dfde51bbae046f0dc2c6e0
Aaron Hatch <aaron@Aarons-MacBook-Pro.local>
2019-05-20 20:20:40 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const friend = "0 > - <";
+
+const friendList = [];
+
+let mySearch = () => {
+    alert('Searching')
+    let createNewFriend = document.createElement('p');
+    document.body.appendChild(createNewFriend);
+    createNewFriend.innerText = friend;
+
+
+}
